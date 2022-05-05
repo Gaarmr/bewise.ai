26ffc5cf12c0c6f61fdc5c4a3c6c5fbdbb60af23
Daniil Romanshen <vdnv@bk.ru>
2022-05-06 05:46:52 +0600

    pep 8 update, add <idea> in gitignore, dockerignore

--- app/config.py
+++ app/config.py
@@ -6,4 +6,5 @@ from pydantic import BaseSettings, Field
 class Settings(BaseSettings):
     db_url: str = Field(..., env='DATABASE_URL')
 
+
 settings = Settings()
--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
 
+
 class QuestionQuery(BaseModel):
     questions_num: int
